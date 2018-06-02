0038a091e64f7e3e0dee25f92ece9dba0640b444
Clément DOUIN <clement.douin@gmail.com>
2018-06-02 16:30:19 +0200

    integrate pricing page

--- src/app/pricing/Pricing.tsx
+++ src/app/pricing/Pricing.tsx
@@ -12,9 +12,10 @@ const campingCar = require("../../static/images/pricing/camping-car.png")
 const trailer = require("../../static/images/pricing/trailer.png")
 const electric = require("../../static/images/pricing/electric.png")
 const tent = require("../../static/images/pricing/tent.png")
-const shower = require("../../static/images/pricing/shower.png")
 const animals = require("../../static/images/pricing/animals.png")
-const coin = require("../../static/images/pricing/coin.png")
+const tax = require("../../static/images/pricing/tax.png")
+const fridge = require("../../static/images/pricing/fridge.png")
+const bungalow = require("../../static/images/pricing/bungalow.png")
 const wifi = require("../../static/images/pricing/wifi.png")
 
 class PricingComponent extends Component<{}, {}> {
@@ -25,146 +26,207 @@ class PricingComponent extends Component<{}, {}> {
   public render() {
     return (
       <div className={styles.pricing}>
-        <div className={styles.pricingColumn}>
-          <div className={styles.pricingRow}>
-            <div className={styles.pricingTitle}>
+        <div className={styles.table}>
+          <div>
+            <div className={styles.title}>
               <h2>Tarifs haute Saison</h2>
               <div>Du 15 mai au 10 septembre</div>
             </div>
 
-            <div className={styles.pricingGridLeft}>
-              <div className={styles.pricingGridRowTeal}>
-                <span className={styles.pricingGridIcon}><img src={adult} alt="Adult pricing" /></span>
-                <span className={styles.pricingGridTitle}>Adulte</span>
-                <span className={styles.pricingGridPrice}>6,10 €/nuitée incl. 0,22 € Taxe de séjour</span>
+            <div className={styles.gridColumnLeft}>
+              <div className={styles.gridRowTeal}>
+                <span className={styles.icon}><img src={adult} alt="Adult pricing" /></span>
+                <span className={styles.itemTeal}>Adulte</span>
+                <span className={styles.item}>6,70 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRowTeal}>
-                <span className={styles.pricingGridIcon}><img src={child} alt="Child pricing" /></span>
-                <span className={styles.pricingGridTitle}>Enfant de 2 à 12 ans</span>
-                <span className={styles.pricingGridPrice}>4,50 €/nuitée</span>
+
+              <div className={styles.gridRowTeal}>
+                <span className={styles.icon}><img src={child} alt="Child pricing" /></span>
+                <span className={styles.itemTeal}>Enfant de 2 à 12 ans</span>
+                <span className={styles.item}>5,10 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRowTeal}>
-                <span className={styles.pricingGridIcon}><img src={baby} alt="Baby pricing" /></span>
-                <span className={styles.pricingGridTitle}>Enfant de - de 2 ans</span>
-                <span className={styles.pricingGridPrice}>gratuit</span>
+
+              <div className={styles.gridRowTeal}>
+                <span className={styles.icon}><img src={baby} alt="Baby pricing" /></span>
+                <span className={styles.itemTeal}>Enfant de - de 2 ans</span>
+                <span className={styles.item}>gratuit</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={car} alt="Car pricing" /></span>
-                <span className={styles.pricingGridTitle}>Voiture</span>
-                <span className={styles.pricingGridPrice}>2,20 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={car} alt="Car pricing" /></span>
+                <span className={styles.itemTeal}>Voiture</span>
+                <span className={styles.item}>2,20 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={moto} alt="Moto pricing" /></span>
-                <span className={styles.pricingGridTitle}>Moto</span>
-                <span className={styles.pricingGridPrice}>1,80 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={moto} alt="Moto pricing" /></span>
+                <span className={styles.itemTeal}>Moto</span>
+                <span className={styles.item}>1,80 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={caravane} alt="Caravane pricing" /></span>
-                <span className={styles.pricingGridTitle}>Caravane</span>
-                <span className={styles.pricingGridPrice}>3 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={caravane} alt="Caravane pricing" /></span>
+                <span className={styles.itemTeal}>Caravane</span>
+                <span className={styles.item}>3 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={miniBus} alt="Mini-bus pricing" /></span>
-                <span className={styles.pricingGridTitle}>Mini Bus</span>
-                <span className={styles.pricingGridPrice}>3 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={miniBus} alt="Mini-bus pricing" /></span>
+                <span className={styles.itemTeal}>Mini Bus</span>
+                <span className={styles.item}>3 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={campingCar} alt="Camping car pricing" /></span>
-                <span className={styles.pricingGridTitle}>Camping car</span>
-                <span className={styles.pricingGridPrice}>5 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={campingCar} alt="Camping car pricing" /></span>
+                <span className={styles.itemTeal}>Camping car</span>
+                <span className={styles.item}>5,50 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={trailer} alt="Trailer pricing" /></span>
-                <span className={styles.pricingGridTitle}>Remorque</span>
-                <span className={styles.pricingGridPrice}>1,50 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={trailer} alt="Trailer pricing" /></span>
+                <span className={styles.itemTeal}>Remorque</span>
+                <span className={styles.item}>1,50 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={electric} alt="Electric pricing" /></span>
-                <span className={styles.pricingGridTitle}>Branchement électrique</span>
-                <span className={styles.pricingGridPrice}>3 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={electric} alt="Electric pricing" /></span>
+                <span className={styles.itemTeal}>Branchement électrique</span>
+                <span className={styles.item}>3 €/nuitée</span>
               </div>
             </div>
           </div>
+
           <div>
-            <div className={styles.pricingTitle}>
+            <div className={styles.title}>
               <h2>Tarifs basse Saison</h2>
               <div>Du 16 avril au 14 mai/du 11 septembre au 21 octobre</div>
             </div>
 
-            <div className={styles.pricingGridRight}>
-              <div className={styles.pricingGridRowTeal}>
-                <span className={styles.pricingGridIcon}><img src={adult} alt="Adult pricing" /></span>
-                <span className={styles.pricingGridTitle}>Adulte</span>
-                <span className={styles.pricingGridPrice}>5,80 €/nuitée incl. 0,22 € Taxe de séjour</span>
+            <div className={styles.gridColumnRight}>
+              <div className={styles.gridRowTeal}>
+                <span className={styles.icon}><img src={adult} alt="Adult pricing" /></span>
+                <span className={styles.itemTeal}>Adulte</span>
+                <span className={styles.item}>5,80 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRowTeal}>
-                <span className={styles.pricingGridIcon}><img src={child} alt="Child pricing" /></span>
-                <span className={styles.pricingGridTitle}>Enfant de 2 à 12 ans</span>
-                <span className={styles.pricingGridPrice}>4,10 €/nuitée</span>
+
+              <div className={styles.gridRowTeal}>
+                <span className={styles.icon}><img src={child} alt="Child pricing" /></span>
+                <span className={styles.itemTeal}>Enfant de 2 à 12 ans</span>
+                <span className={styles.item}>4,10 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRowTeal}>
-                <span className={styles.pricingGridIcon}><img src={baby} alt="Baby pricing" /></span>
-                <span className={styles.pricingGridTitle}>Enfant de - de 2 ans</span>
-                <span className={styles.pricingGridPrice}>gratuit</span>
+
+              <div className={styles.gridRowTeal}>
+                <span className={styles.icon}><img src={baby} alt="Baby pricing" /></span>
+                <span className={styles.itemTeal}>Enfant de - de 2 ans</span>
+                <span className={styles.item}>gratuit</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={car} alt="Car pricing" /></span>
-                <span className={styles.pricingGridTitle}>Voiture</span>
-                <span className={styles.pricingGridPrice}>2 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={car} alt="Car pricing" /></span>
+                <span className={styles.itemTeal}>Voiture</span>
+                <span className={styles.item}>2 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={moto} alt="Moto pricing" /></span>
-                <span className={styles.pricingGridTitle}>Moto</span>
-                <span className={styles.pricingGridPrice}>1,50 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={moto} alt="Moto pricing" /></span>
+                <span className={styles.itemTeal}>Moto</span>
+                <span className={styles.item}>1,50 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={caravane} alt="Caravane pricing" /></span>
-                <span className={styles.pricingGridTitle}>Caravane</span>
-                <span className={styles.pricingGridPrice}>2,50 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={caravane} alt="Caravane pricing" /></span>
+                <span className={styles.itemTeal}>Caravane</span>
+                <span className={styles.item}>2,50 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={miniBus} alt="Mini-bus pricing" /></span>
-                <span className={styles.pricingGridTitle}>Mini Bus</span>
-                <span className={styles.pricingGridPrice}>2,50 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={miniBus} alt="Mini-bus pricing" /></span>
+                <span className={styles.itemTeal}>Mini Bus</span>
+                <span className={styles.item}>2,50 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={campingCar} alt="Camping car pricing" /></span>
-                <span className={styles.pricingGridTitle}>Camping car</span>
-                <span className={styles.pricingGridPrice}>4 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={campingCar} alt="Camping car pricing" /></span>
+                <span className={styles.itemTeal}>Camping car</span>
+                <span className={styles.item}>4 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={trailer} alt="Trailer pricing" /></span>
-                <span className={styles.pricingGridTitle}>Remorque</span>
-                <span className={styles.pricingGridPrice}>1,30 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={trailer} alt="Trailer pricing" /></span>
+                <span className={styles.itemTeal}>Remorque</span>
+                <span className={styles.item}>1,30 €/nuitée</span>
               </div>
-              <div className={styles.pricingGridRow}>
-                <span className={styles.pricingGridIcon}><img src={electric} alt="Electric pricing" /></span>
-                <span className={styles.pricingGridTitle}>Branchement électrique</span>
-                <span className={styles.pricingGridPrice}>3 €/nuitée</span>
+
+              <div className={styles.gridRow}>
+                <span className={styles.icon}><img src={electric} alt="Electric pricing" /></span>
+                <span className={styles.itemTeal}>Branchement électrique</span>
+                <span className={styles.item}>3 €/nuitée</span>
               </div>
             </div>
           </div>
         </div>
 
-        <div>
-          <div>
-            <span className={styles.pricingGridIcon}><img src={tent} alt="Tent pricing" /></span>
-            <span className={styles.pricingGridTitle}>L'emplacement et la tente sont gratuits</span>
-          </div>
+        <div className={styles.table}>
+          <div className={styles.gridTeal}>
+            <div className={styles.columnLeft}>
+              <div className={styles.row}>
+                <span className={styles.icon}><img src={tent} alt="Tent pricing" /></span>
+                <span className={styles.item}>L'emplacement et la tente sont gratuits</span>
+              </div>
 
-          <div>
-            <span className={styles.pricingGridIcon}><img src={shower} alt="Shower pricing" /></span>
-            <span className={styles.pricingGridTitle}>Les douches chaudes sont gratuites</span>
-          </div>
+              <div className={styles.row}>
+                <span className={styles.icon}><img src={tax} alt="Tax pricing" /></span>
+                <span className={styles.item}>Taxe de séjour inclue</span>
+              </div>
 
-          <div>
-            <span className={styles.pricingGridIcon}><img src={animals} alt="Animals pricing" /></span>
-            <span className={styles.pricingGridTitle}>Les animaux sont acceptés, leur séjour est gratuit</span>
-          </div>
+              <div className={styles.row}>
+                <span className={styles.icon}><img src={animals} alt="Animals pricing" /></span>
+                <span className={styles.item}>Les animaux sont acceptés, leur séjour est gratuit</span>
+              </div>
+            </div>
 
-          <div>
-            <span className={styles.pricingGridIcon}><img src={coin} alt="Wash machine coins pricing" /></span>
-            <span className={styles.pricingGridTitle}>Jeton de machine à laver : 5 € (lessive fournie)</span>
+            <div className={styles.columnRight}>
+              <div className={styles.row}>
+                <span className={styles.icon}><img src={fridge} alt="Fridge pricing" /></span>
+                <span className={styles.item}>Location de frigos : 3 €/nuitée</span>
+              </div>
+
+              <div className={styles.row}>
+                <span className={styles.icon}><img src={wifi} alt="WiFi pricing" /></span>
+                <span className={styles.itemWifi}>WiFi :</span>
+                <span className={styles.itemWifiInfo}>
+                  <span>1 Jour</span>
+                  <span>3 Jours</span>
+                  <span>7 Jours</span>
+                </span>
+                <span className={styles.itemWifiInfo}>
+                  <span>-></span>
+                  <span>-></span>
+                  <span>-></span>
+                </span>
+                <span className={styles.itemWifiInfo}>
+                  <span>2€</span>
+                  <span>5€</span>
+                  <span>10€</span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.table}>
+          <div className={styles.gridTeal}>
+            <div className={styles.column}>
+              <div className={styles.row}>
+                <span className={styles.iconBungalow}><img src={bungalow} alt="Bungalow pricing" /></span>
+                <span className={styles.itemBungalow}>Location de bungalows :</span>
+                <span className={styles.item}>
+                  <span>Du 01 avril au 26 mai et du 22 septembre au 26 octobre : 280€/semaine</span>
+                  <span>Du 26 mai au 07 juillet et du 25 août au 22 septembre : 400€/semaine</span>
+                  <span>Du 07 juillet au 25 août : 600€/semaine</span>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
