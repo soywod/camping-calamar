f1422f733a899a228a10fe2bf6906307427e5d3f
Clément DOUIN <clement.douin@gmail.com>
2018-06-03 12:46:57 +0200

    improve navbar ux on mobile

--- src/app/navigation/Navbar.tsx
+++ src/app/navigation/Navbar.tsx
@@ -1,9 +1,9 @@
 import React, {Component} from "react"
 import {Link, Route} from "react-router-dom"
+import Swipeable from "react-swipeable"
 
 const logo = require("../../static/images/logo-camping-calamar.png")
 const burger = require("../../static/images/navbar/burger.png")
-const cross = require("../../static/images/navbar/cross.png")
 const logoPresentationOn = require("../../static/images/logo-presentation-on.png")
 const logoPresentationOff = require("../../static/images/logo-presentation-off.png")
 const logoPricingOn = require("../../static/images/logo-pricing-on.png")
@@ -33,103 +33,107 @@ class NavbarComponent extends Component<{}, IState> {
     }
   }
 
+  public renderHomeLink() {
+    const link = (
+      <Link to="/" className={styles.logo} onClick={this.closeNavbar}>
+        <img width="100%" src={logo} alt="Camping Calamar logo" />
+      </Link>
+    )
+
+    return <Route exact path="/" children={link} />
+  }
+
   public render() {
     return (
-      <>
-        <img className={styles.burgerOpen} src={burger} alt="Open menu" onClick={this.openNavbar} />
-        <nav className={styles.nav} data-open={this.state.opened}>
-          <img className={styles.burgerClose} src={cross} alt="Close menu" onClick={this.closeNavbar} />
-          <Route
-            exact
-            path="/"
-            children={() => (
-              <Link to="/" className={styles.logo} onClick={this.closeNavbar}>
-                <img width="100%" src={logo} alt="Camping Calamar logo" />
-              </Link>
-            )}
-          />
+      <React.Fragment>
+        <img className={styles.burger} src={burger} alt="Open menu" onMouseDown={this.openNavbar} />
+        <div className={styles.fade} data-visible={this.state.opened} onMouseDown={this.closeNavbar} />
+        <Swipeable onSwipingLeft={this.closeNavbar}>
+          <nav className={styles.nav} data-visible={this.state.opened}>
+            {this.renderHomeLink()}
 
-          <div>
-            <Route
-              exact
-              path="/presentation"
-              children={({match}) => (
-                <Link to="/presentation" className={styles.item} data-match={!!match} onClick={this.closeNavbar}>
-                  <div className={styles.itemLogo}>
-                    <img className={styles.on} src={logoPresentationOn} alt="Presentation icon" />
-                    <img className={styles.off} src={logoPresentationOff} alt="Presentation icon" />
-                  </div>
+            <div>
+              <Route
+                exact
+                path="/presentation"
+                children={({match}) => (
+                  <Link to="/presentation" className={styles.item} data-match={!!match} onClick={this.closeNavbar}>
+                    <div className={styles.itemLogo}>
+                      <img className={styles.on} src={logoPresentationOn} alt="Presentation icon" />
+                      <img className={styles.off} src={logoPresentationOff} alt="Presentation icon" />
+                    </div>
 
-                  <span>Présentation</span>
-                </Link>
-              )}
-            />
+                    <span>Présentation</span>
+                  </Link>
+                )}
+              />
 
-            <Route
-              exact
-              path="/pricing"
-              children={({match}) => (
-                <Link to="/pricing" className={styles.item} data-match={!!match} onClick={this.closeNavbar}>
-                  <div className={styles.itemLogo}>
-                    <img className={styles.on} src={logoPricingOn} alt="Pricing icon" />
-                    <img className={styles.off} src={logoPricingOff} alt="Pricing icon" />
-                  </div>
-                  <span>Infos & tarifs</span>
-                </Link>
-              )}
-            />
+              <Route
+                exact
+                path="/pricing"
+                children={({match}) => (
+                  <Link to="/pricing" className={styles.item} data-match={!!match} onClick={this.closeNavbar}>
+                    <div className={styles.itemLogo}>
+                      <img className={styles.on} src={logoPricingOn} alt="Pricing icon" />
+                      <img className={styles.off} src={logoPricingOff} alt="Pricing icon" />
+                    </div>
+                    <span>Infos & tarifs</span>
+                  </Link>
+                )}
+              />
 
-            <Route
-              exact
-              path="/booking"
-              children={({match}) => (
-                <Link to="/booking" className={styles.item} data-match={!!match} onClick={this.closeNavbar}>
-                  <div className={styles.itemLogo}>
-                    <img className={styles.on} src={logoBookingOff} alt="Booking icon" />
-                    <img className={styles.off} src={logoBookingOff} alt="Booking icon" />
-                  </div>
-                  <span>Réservation</span>
-                </Link>
-              )}
-            />
+              <Route
+                exact
+                path="/booking"
+                children={({match}) => (
+                  <Link to="/booking" className={styles.item} data-match={!!match} onClick={this.closeNavbar}>
+                    <div className={styles.itemLogo}>
+                      <img className={styles.on} src={logoBookingOff} alt="Booking icon" />
+                      <img className={styles.off} src={logoBookingOff} alt="Booking icon" />
+                    </div>
+                    <span>Réservation</span>
+                  </Link>
+                )}
+              />
 
-            <Route
-              exact
-              path="/area"
-              children={({match}) => (
-                <Link to="/area" className={styles.item} data-match={!!match} onClick={this.closeNavbar}>
-                  <div className={styles.itemLogo}>
-                    <img className={styles.on} src={logoAreaOn} alt="Area icon" />
-                    <img className={styles.off} src={logoAreaOff} alt="Area icon" />
-                  </div>
-                  <span>Alentours</span>
-                </Link>
-              )}
-            />
+              <Route
+                exact
+                path="/area"
+                children={({match}) => (
+                  <Link to="/area" className={styles.item} data-match={!!match} onClick={this.closeNavbar}>
+                    <div className={styles.itemLogo}>
+                      <img className={styles.on} src={logoAreaOn} alt="Area icon" />
+                      <img className={styles.off} src={logoAreaOff} alt="Area icon" />
+                    </div>
+                    <span>Alentours</span>
+                  </Link>
+                )}
+              />
 
-            <Route
-              exact
-              path="/contact"
-              children={({match}) => (
-                <Link to="/contact" className={styles.item} data-match={!!match} onClick={this.closeNavbar}>
-                  <div className={styles.itemLogo}>
-                    <img className={styles.on} src={logoContactOn} alt="Contact icon" />
-                    <img className={styles.off} src={logoContactOff} alt="Contact icon" />
-                  </div>
-                  <span>Contact/plan</span>
-                </Link>
-              )}
-            />
-          </div>
+              <Route
+                exact
+                path="/contact"
+                children={({match}) => (
+                  <Link to="/contact" className={styles.item} data-match={!!match} onClick={this.closeNavbar}>
+                    <div className={styles.itemLogo}>
+                      <img className={styles.on} src={logoContactOn} alt="Contact icon" />
+                      <img className={styles.off} src={logoContactOff} alt="Contact icon" />
+                    </div>
+                    <span>Contact/plan</span>
+                  </Link>
+                )}
+              />
+            </div>
 
-          <div className={styles.lang}>
-            <a href="#" className={styles.langItem}><img src={flagFR} alt="French translation" /></a>
-            <a href="#" className={styles.langItem}><img src={flagEN} alt="English translation" /></a>
-            <a href="#" className={styles.langItem}><img src={flagDE} alt="German translation" /></a>
-            <a href="#" className={styles.langItem}><img src={flagIT} alt="Italian translation" /></a>
-          </div>
-        </nav>
-      </>
+            <div className={styles.lang}>
+              <a href="#" className={styles.langItem}><img src={flagFR} alt="French translation" /></a>
+              <a href="#" className={styles.langItem}><img src={flagEN} alt="English translation" /></a>
+              <a href="#" className={styles.langItem}><img src={flagDE} alt="German translation" /></a>
+              <a href="#" className={styles.langItem}><img src={flagIT} alt="Italian translation" /></a>
+            </div>
+          </nav>
+        </Swipeable>
+      </React.Fragment>
     )
   }
 
