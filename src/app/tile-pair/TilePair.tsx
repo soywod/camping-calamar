9224ad2493ad311dffad8f89227bd9eedeb4623f
Clément DOUIN <clement.douin@gmail.com>
2018-06-20 02:06:35 +0200

    add ui animation on appearing tile pair

--- src/app/tile-pair/TilePair.tsx
+++ src/app/tile-pair/TilePair.tsx
@@ -2,17 +2,51 @@ import React, {Component} from "react"
 
 const styles = require("./TilePair.sass")
 
-class TilePairComponent extends Component<{}, {}> {
+interface IState {
+  visible: boolean
+}
+
+class TilePairComponent extends Component<{}, IState> {
+  private $container: HTMLDivElement | null
+
   constructor(props: {}) {
     super(props)
+
+    this.state = {
+      visible: false,
+    }
   }
 
   public render() {
+    const {visible} = this.state
+    const {tilePair} = styles
+    const {children} = this.props
+
     return (
-      <div className={styles.tilePair}>
-        {this.props.children}
+      <div ref={(ref) => this.$container = ref} className={tilePair} data-visible={visible}>
+        {children}
       </div>
     )
+  }
+
+  public componentDidMount() {
+    window.addEventListener("scroll", this.checkVisibility)
+  }
+
+  public componentWillUnmount() {
+    window.removeEventListener("scroll", this.checkVisibility)
+  }
+
+  private checkVisibility = () => {
+    if (! this.$container) {
+      return
+    }
+
+    const wheight = window.innerHeight * .7
+    const wscroll = window.scrollY
+    const visible = wheight + wscroll > this.$container.offsetTop
+
+    this.setState({visible})
   }
 }
 
